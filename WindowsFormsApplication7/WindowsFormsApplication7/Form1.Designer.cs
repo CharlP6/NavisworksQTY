6abda9d19e25fb2b6f5db1435e757d21d93a32fa
CharlP6 <pretorius.charl6@gmail.com>
2016-08-02 15:35:14 +0200

    First

---
+++ WindowsFormsApplication7/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstTables = new System.Windows.Forms.ListBox();
+            this.lstColumns = new System.Windows.Forms.ListBox();
+            this.lstGroups = new System.Windows.Forms.ListBox();
+            this.lstHeaders = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(452, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstTables
+            // 
+            this.lstTables.FormattingEnabled = true;
+            this.lstTables.Location = new System.Drawing.Point(12, 12);
+            this.lstTables.Name = "lstTables";
+            this.lstTables.Size = new System.Drawing.Size(120, 238);
+            this.lstTables.TabIndex = 1;
+            this.lstTables.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lstColumns
+            // 
+            this.lstColumns.FormattingEnabled = true;
+            this.lstColumns.Location = new System.Drawing.Point(138, 12);
+            this.lstColumns.Name = "lstColumns";
+            this.lstColumns.Size = new System.Drawing.Size(120, 238);
+            this.lstColumns.TabIndex = 2;
+            // 
+            // lstGroups
+            // 
+            this.lstGroups.FormattingEnabled = true;
+            this.lstGroups.Location = new System.Drawing.Point(318, 12);
+            this.lstGroups.Name = "lstGroups";
+            this.lstGroups.Size = new System.Drawing.Size(120, 95);
+            this.lstGroups.TabIndex = 3;
+            // 
+            // lstHeaders
+            // 
+            this.lstHeaders.FormattingEnabled = true;
+            this.lstHeaders.Location = new System.Drawing.Point(318, 155);
+            this.lstHeaders.Name = "lstHeaders";
+            this.lstHeaders.Size = new System.Drawing.Size(120, 95);
+            this.lstHeaders.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(264, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "<<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(578, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(452, 12);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(201, 95);
+            this.listBox5.TabIndex = 8;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(12, 324);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(984, 355);
+            this.listBox6.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 692);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lstHeaders);
+            this.Controls.Add(this.lstGroups);
+            this.Controls.Add(this.lstColumns);
+            this.Controls.Add(this.lstTables);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstTables;
+        private System.Windows.Forms.ListBox lstColumns;
+        private System.Windows.Forms.ListBox lstGroups;
+        private System.Windows.Forms.ListBox lstHeaders;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+    }
+}
+
---
+++ WindowsFormsApplication7/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ExcelImport;
+
+        #region Excel
+
+        private string GetConnectionString()
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+            // XLSX - Excel 2007, 2010, 2012, 2013
+            props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+            props["Extended Properties"] = "Excel 12.0 XML";
+            props["Data Source"] = @"D:\Users\pretoriusc\Desktop\Project Temp Folder\Navisworks QTY\NW qty Export.xlsx";
+
+            // XLS - Excel 2003 and Older
+            //props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+            //props["Extended Properties"] = "Excel 8.0";
+            //props["Data Source"] = "C:\\MyExcel.xls";
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        private DataSet ReadExcelFile()
+        {
+            DataSet ds = new DataSet();
+
+            string connectionString = GetConnectionString();
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    OleDbCommand cmd = new OleDbCommand();
+                    cmd.Connection = conn;
+                    DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                    foreach (DataRow dr in dtSheet.Rows)
+                    {
+                        string sheetName = dr["TABLE_NAME"].ToString();
+
+                        cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+                        cmd.Parameters.AddWithValue("TableName", sheetName);
+
+                        DataTable dt = new DataTable();
+                        dt.TableName = sheetName.Replace("\'", "").Replace("$", "");
+
+                        try
+                        {
+                            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                            da.Fill(dt);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+                        ds.Tables.Add(dt);
+                    }
+
+                    cmd = null;
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);                
+                }
+            }
+            return ds;
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExcelImport = ReadExcelFile();
+
+            foreach (DataTable dt in ExcelImport.Tables)
+            {
+                lstTables.Items.Add(dt.TableName);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstColumns.Items.Clear();
+
+
+            foreach (DataColumn dc in ExcelImport.Tables[lstTables.SelectedItem.ToString()].Columns)
+            {
+                lstColumns.Items.Add(dc.ColumnName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lstGroups.Items.Add(lstColumns.SelectedItem.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox6.Items.Clear();
+
+            //create a real list of data rows
+            List<DataRow> TableRows = new List<DataRow>();
+            foreach(DataRow dr in ExcelImport.Tables[lstTables.SelectedItem.ToString()].Rows)
+            {
+                TableRows.Add(dr);
+            }
+
+            #region Initialize Iteration Variables
+            int Level = 0;
+            int maxLevel = lstGroups.Items.Count - 1;
+            int MaxSize = lstGroups.Items.Count;
+
+            Queue<string>[] DataTree = new Queue<string>[MaxSize];
+            for (int i = 0; i <= maxLevel; i++)
+                DataTree[i] = new Queue<string>();
+
+            string[] LevelString = new string[MaxSize];
+
+            string[] Group = new string[MaxSize];
+
+            for (int i = 0; i < MaxSize; i++)
+                Group[i] = lstGroups.Items[i].ToString();
+            #endregion
+
+            TableRows.Select(s => s[Group[Level]] == DBNull.Value ? "" : (string)s[Group[Level]]).Distinct().ToList().ForEach(f => DataTree[Level].Enqueue(f)); //Populate first stack
+            
+
+            while(Level >= 0)
+            {
+                if (DataTree[Level].Count > 0)
+                {
+
+                    LevelString[Level] = DataTree[Level].Dequeue();
+
+                    IEnumerable<DataRow> StackEnum = TableRows.Where(w => ((w[Group[0]] == DBNull.Value) ? "" : (string)w[Group[0]]) == LevelString[0]);
+
+                    for (int i = 1; i <= Level; i++)
+                    {
+                        string ls = LevelString[i];
+                        string gs = Group[i];
+
+                        StackEnum = StackEnum.Where(w => (w[gs] == DBNull.Value ? "" : (string)w[gs]) == ls);
+
+
+                    }
+
+                    if (Level < maxLevel)
+                    {
+                        List<string> tList = StackEnum.Select(s => s[Group[Level + 1]] == DBNull.Value ? "" : (string)s[Group[Level + 1]]).Distinct().ToList();
+                        tList.ForEach(f => DataTree[Level + 1].Enqueue(f));
+
+                        if (LevelString[Level] != "")
+                        {
+                            int lvl = LevelString.Take(Level + 1).Where(w => w == "").Count();
+                            listBox6.Items.Add((Level - lvl).ToString() + ": " + LevelString[Level]);
+                        }
+                    }
+                    else
+                    {
+                        int lvl = 0;
+                        if (LevelString[Level] != "")
+                        {
+                            lvl = LevelString.Take(Level + 1).Where(w => w == "").Count();
+                        }
+
+                        listBox6.Items.Add((Level - lvl).ToString() + ": " + LevelString[Level] + "\t" + StackEnum.Sum(su => su["PrimaryQuantity"] == DBNull.Value ? 0 : (double)su["PrimaryQuantity"]).ToString());
+                    }
+
+
+
+                    Level = (Level + 1) > maxLevel ? maxLevel : Level + 1;
+
+                }
+                else
+                {
+                    Level -= 1;
+                }
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lstGroups.Items.RemoveAt(lstGroups.SelectedIndex);
+        }
+
+    }
+
+    class Hierarchy
+    {
+        public int Level { get; set; }
+        public string Name { get; set; }
+    }
+}
